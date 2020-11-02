9eed0720ec3b237a4400f92284e2470882131939
Tue Hellstern <tue@f1-support.dk>
2020-11-02 17:12:31 +0100

    Update PowerPoint.R

--- PowerPoint.R
+++ PowerPoint.R
@@ -2,24 +2,8 @@
 
 # Pakker
 install.packages("officer")
-install.packages("flextable")
-
 library(officer)
-library(flextable)
 
-# Opret Flextable - SalesByCountryTable - Top 5 country
-SalesByCountryTop5 <- head(SalesByCountry, 5)
-
-SalesByCountryTable <- flextable(
-  head(SalesByCountryTop5),
-  col_keys = c("Country", "Total", "Bar"))
-
-num_keys <- c("Total")
-SalesByCountryTable <- colformat_num(x = SalesByCountryTable, j = num_keys, big.mark = ".", decimal.mark = ",", digits = 0)
-SalesByCountryTable <- autofit(SalesByCountryTable)
-
-
-SalesByCountryTable
 
 # Opret PowerPoint objekt
 PowerPointSalgsRapport <- read_pptx()
@@ -29,14 +13,10 @@ PowerPointSalgsRapport <- PowerPointSalgsRapport %>%
   add_slide(layout = "Title Only", master = "Office Theme") %>% 
   ph_with(value = "Sales rapport", location = ph_location_type(type = "title")) %>% 
   ph_with(value = "Tue Hellstern", location = ph_location_type(type = "ftr")) %>% 
-  # Slide Table
-  add_slide(layout = "Title and Content") %>% 
-  ph_with(value = "Top 5 sales by country", location = ph_location_type(type = "title")) %>%
-  ph_with(value = SalesByCountryTable, location = ph_location_type(type = "body")) %>% 
   # Slide Plot
   add_slide(layout = "Title and Content") %>% 
   ph_with(value = "Sales by country", location = ph_location_type(type = "title")) %>%
-  ph_with(value = SalesByCountryPlot, location = ph_location_type(type = "body")) %>% 
+  ph_with(value = SalesByCountryTop5Plot, location = ph_location_type(type = "body")) %>% 
   # Slide Plot
   add_slide(layout = "Title and Content") %>% 
   ph_with(value = "Sales by weekday", location = ph_location_type(type = "title")) %>%
